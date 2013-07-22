0535a00950ded835bcdc1b1ab765acdcad1c6f02
tinyladi <ladislas@weareleka.com>
2013-07-22 16:55:59 +0200

    cleanup

--- public/js/data.js
+++ public/js/data.js
@@ -6,21 +6,21 @@ function load() {
 
 		data = JSON.parse(data.value.replace(/ /g,''));
 
-		x = document.getElementById("x");
-		y = document.getElementById("y");
-		z = document.getElementById("z");
-
-		yaw = document.getElementById("yaw");
+		x     = document.getElementById("x");
+		y     = document.getElementById("y");
+		z     = document.getElementById("z");
+		
+		yaw   = document.getElementById("yaw");
 		pitch = document.getElementById("pitch");
-		roll = document.getElementById("roll");
+		roll  = document.getElementById("roll");
 
-		x.innerHTML = data.accel.x;
-		y.innerHTML = data.accel.y;
-		z.innerHTML = data.accel.z;
-
+		x.innerHTML     = data.accel.x;
+		y.innerHTML     = data.accel.y;
+		z.innerHTML     = data.accel.z;
+		
 		pitch.innerHTML = data.gyro.pitch;
-		yaw.innerHTML = data.gyro.yaw;
-		roll.innerHTML = data.gyro.roll;
+		yaw.innerHTML   = data.gyro.yaw;
+		roll.innerHTML  = data.gyro.roll;
 
 	});
 }	
