b0e131d035a95c129506f850df333bec33e383e8
tinyladi <ladislas@weareleka.com>
2013-07-22 16:55:19 +0200

    cleanup

--- public/js/data.js
+++ public/js/data.js
@@ -3,21 +3,26 @@ function load() {
 	var socket = io.connect('http://localhost:8080');
 	// when you get a serialdata event, do this:
 	socket.on('serialEvent', function (data) {
-	data = JSON.parse(data.value.replace(/ /g,""));
-	x = document.getElementById("x");
-	y = document.getElementById("y");
-	z = document.getElementById("z");
-	yaw = document.getElementById("yaw");
-	pitch = document.getElementById("pitch");
-	roll = document.getElementById("roll");
-       x.innerHTML = data.accel.x;
-       y.innerHTML = data.accel.y;
-       z.innerHTML = data.accel.z;
-       pitch.innerHTML = data.gyro.pitch;
-       yaw.innerHTML = data.gyro.yaw;
-       roll.innerHTML = data.gyro.roll;
-		
-		});
-	}	
+
+		data = JSON.parse(data.value.replace(/ /g,''));
+
+		x = document.getElementById("x");
+		y = document.getElementById("y");
+		z = document.getElementById("z");
+
+		yaw = document.getElementById("yaw");
+		pitch = document.getElementById("pitch");
+		roll = document.getElementById("roll");
+
+		x.innerHTML = data.accel.x;
+		y.innerHTML = data.accel.y;
+		z.innerHTML = data.accel.z;
+
+		pitch.innerHTML = data.gyro.pitch;
+		yaw.innerHTML = data.gyro.yaw;
+		roll.innerHTML = data.gyro.roll;
+
+	});
+}	
 
 window.addEventListener("load", load, true);
