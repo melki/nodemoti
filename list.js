dd78b664e21ec3e423c7e3adfc14a74a10b7df42
tinyladi <ladislas@weareleka.com>
2013-07-26 19:54:44 +0200

    add all the elements

--- list.js
+++ list.js
@@ -2,7 +2,7 @@
 	Lists serial ports. Copied from https://github.com/voodootikigod/node-serialport readme
 */
 var serialport = require("serialport"),       // include the serialport library
-  SerialPort  = serialport.SerialPort;    
+  SerialPort  = serialport.SerialPort;
 
 
 
@@ -10,12 +10,20 @@ var serialport = require("serialport"),       // include the serialport library
 
 serialport.list(function (err, ports) {
 	ports.forEach(function(port) {
-		if(port.pnpId.indexOf("duino") != -1 || port.manufacturer.indexOf("duino") != -1){
+
+		console.log("pnpId: " + port.pnpId);
+		console.log("manufacturer: " + port.manufacturer);
+		console.log("comName: " + port.comName);
+		console.log("serialNumber: " + port.serialNumber);
+		console.log("vendorId: " + port.vendorId);
+		console.log("productId: " + port.productId);
+
+		if(port.pnpId.indexOf("duino") != -1 || port.manufacturer.indexOf("duino") != -1 || port.comName.indexOf('moti') != -1){
 				myPort = new SerialPort(port.comName,{
 				baudrate: 115200,
 				parser: serialport.parsers.readline("\r\n"),
 			});
-				console.log("Ok for port "+ port.comName);	
+				console.log("Ok for port "+ port.comName);
 		}
 	});
 });
